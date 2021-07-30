a2fab65e6edb0bfef4e0a444ca46d21f3e8fea37
Connie Hyman <connie2630a@gmail.com>
2021-07-30 12:32:56 -0700

    Add validation to event_type

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ApplicationRecord
   validates :name, :event_type, presence: true
+  validates :event_type, inclusion: { in: %w(view click scroll submit),
+    message: "%{value} is not a valid event_type" }
 end
