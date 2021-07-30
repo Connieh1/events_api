b1b1d5b2ee7af671d55205d6b823947ffff5ead8
Connie Hyman <connie2630a@gmail.com>
2021-07-30 12:34:40 -0700

    Update event_type validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
   validates :name, :event_type, presence: true
-  validates :event_type, inclusion: { in: %w(view click scroll submit),
+  validates :event_type, inclusion: { in: %w(view click scroll submit play),
     message: "%{value} is not a valid event_type" }
 end
